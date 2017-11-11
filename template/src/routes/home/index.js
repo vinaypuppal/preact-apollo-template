242457f0b8a51a6f0d7e515dea089bb1f3928862
Vinay Puppal <me@vinaypuppal.com>
2017-11-11 16:37:18 +0530

    fix typo

--- template/src/routes/home/index.js
+++ template/src/routes/home/index.js
@@ -5,21 +5,21 @@ import style from './style';
 import LOCATION_QUERY from '../../graphql/location.gql';
 
 const weatherCard = data => (
-	<div className="map">
-		<h1>Weather for Hyderabad, {data.country}</h1>
-		Map: <a href={data.mapLink}>Hyderabad, {data.country}</a>
-		<p>
-			{data.weather.temperature}deg ({data.weather.summary})
-		</p>
-	</div>
+  <div className="map">
+    <h1>Weather for Sydney, {data.country}</h1>
+    Map: <a href={data.mapLink}>Sydney, {data.country}</a>
+    <p>
+      {data.weather.temperature}deg ({data.weather.summary})
+    </p>
+  </div>
 );
 
 export default graphql(LOCATION_QUERY)(props => (
-	<div className={style.home}>
-		<div className="container">
-			<div className="content">
-				{props.data.loading ? <p>Loading...</p> : weatherCard(props.data.location)}
-			</div>
-		</div>
-	</div>
+  <div className={style.home}>
+    <div className="container">
+      <div className="content">
+        {props.data.loading ? <p>Loading...</p> : weatherCard(props.data.location)}
+      </div>
+    </div>
+  </div>
 ));
